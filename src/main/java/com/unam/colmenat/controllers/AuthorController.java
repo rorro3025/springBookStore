19d5e81e63d2ecfdfa237012e93287496cde10dc
Rodrigo <rorro3025@outlook.com>
2022-07-02 02:30:58 -0500

    Controller distibution * author * person

--- src/main/java/com/unam/colmenat/controllers/AuthorController.java
+++ src/main/java/com/unam/colmenat/controllers/AuthorController.java
@@ -1,2 +1,45 @@
-package com.unam.colmenat.controllers;public class AuthorController {
+package com.unam.colmenat.controllers;
+
+import com.unam.colmenat.entities.Author;
+import com.unam.colmenat.services.AuthorServiceImp;
+import com.unam.colmenat.services.PersonServiceImp;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/api/author")
+public class AuthorController {
+
+    private final AuthorServiceImp personService;
+
+    public AuthorController(AuthorServiceImp personService) {
+        this.personService = personService;
+    }
+
+    @GetMapping("")
+    public List<Author> getAll() throws Exception{
+       return personService.findAll();
+    }
+
+    @GetMapping("/{id}")
+   public Author getById(@PathVariable Long id) throws Exception{
+       return personService.findById(id);
+   }
+
+   @PostMapping("")
+    public Author create(@RequestBody Author author) throws Exception{
+        return personService.save(author);
+   }
+
+   @PutMapping("/update")
+    public Author update(@RequestBody Author author) throws Exception{
+       return personService.update(author);
+   }
+
+   @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) throws Exception{
+       personService.delete(id);
+   }
 }
--- src/main/java/com/unam/colmenat/controllers/PeopleController.java
+++ src/main/java/com/unam/colmenat/controllers/PeopleController.java
@@ -1,6 +1,7 @@
 package com.unam.colmenat.controllers;
 
 import com.unam.colmenat.entities.Person;
+import com.unam.colmenat.services.PersonService;
 import com.unam.colmenat.services.PersonServiceImp;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,5 +10,37 @@ import java.util.List;
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/people")
-public class PeopleController extends BaseControllerImpl<Person,PersonServiceImp>{
+public class PeopleController {
+
+    private final PersonService personService;
+
+    public PeopleController(PersonServiceImp personService){
+        this.personService = personService;
+    }
+
+    @GetMapping("")
+    public List<Person> getAll() throws Exception{
+        return personService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Person getById(@PathVariable Long id) throws Exception{
+        return personService.findById(id);
+    }
+
+    @PostMapping("")
+    public Person create(@RequestBody Person person) throws Exception{
+        return personService.save(person);
+    }
+
+    @PutMapping("/update")
+    public Person update(@RequestBody Person person) throws Exception{
+        return personService.update(person);
+    }
+
+   @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) throws Exception{
+        personService.delete(id);
+    }
+
 }
